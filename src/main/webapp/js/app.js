7434b0ae076c30ce4c92f53f3152321d84b3afe1
DineshaKarunathilake <dineshakarunathilake@gmail.com>
2016-10-20 12:21:00 +0530

    Upto loading batchlist

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -6,38 +6,55 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xed
 				templateUrl: 'partials/create.html',
 				controller: CreateController
 			});
+			$routeProvider.when('/selectStage', {
+            				templateUrl: 'partials/selectStage.html',
+            				controller: SelectStageController
+            			});
+
+            $routeProvider.when('/beforePresetting', {
+                        	templateUrl: 'partials/beforePresetting.html',
+                        	controller: BeforePresettingController
+                        });
+
+            $routeProvider.when('/afterPresetting', {
+                        	templateUrl: 'partials/afterPresetting.html',
+                        	controller: AfterPresettingController
+                        });
+
+              $routeProvider.when('/afterDyeing', {
+                            templateUrl: 'partials/afterDyeing.html',
+                            controller: AfterDyeingController
+                        });
 			$routeProvider.when('/newmeasurement', {
-                 templateUrl: 'partials/newmeasurement.html',
-                 controller: NewMeasurementController
-            });
+                            templateUrl: 'partials/newmeasurement.html',
+                            controller: NewMeasurementController
+                        });
 
-			$routeProvider.when('/edit/:id', {
-				templateUrl: 'partials/edit.html',
-				controller: EditController
-			});
 
-			$routeProvider.when('/login', {
-				templateUrl: 'partials/login.html',
-				controller: LoginController
-
-			});
-			
             $routeProvider.when('/addBody', {
                             templateUrl: 'partials/addBody.html',
                             controller: EditableTableCtrl
 
                         });
 
-            $routeProvider.when('/addSleeve', {
+            $routeProvider.when('/addSleeve',
+                        {
                              templateUrl: 'partials/addSleeve.html',
                              controller: AddSleeveCtrl
 
-                                    });
+                        });
+
+            $routeProvider.when('/batchList',
+                         {
+                             templateUrl: 'partials/batchList.html',
+                             controller: BatchListCtrl
+
+                         });
 
 			$routeProvider.otherwise(
             			{
-                            templateUrl: 'partials/viewmeasurements.html',
-                        	controller: ViewMeasurementsController
+                            templateUrl: 'partials/firstpage.html',
+                        	controller: FirstPageController
                         });
 
 
@@ -54,7 +71,7 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xed
 			        		var url = config.url;
 			      
 			        		if (status == 401) {
-			        			$location.path( "/login" );
+			        			$location.path( "/firstpage" );
 			        		} else {
 			        			$rootScope.error = method + " on " + url + " failed with status " + status;
 			        		}
@@ -71,14 +88,7 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xed
 		        return {
 		        	'request': function(config) {
 		        		var isRestCall = config.url.indexOf('rest') == 0;
-		        		if (isRestCall && angular.isDefined($rootScope.accessToken)) {
-		        			var accessToken = $rootScope.accessToken;
-		        			if (exampleAppConfig.useAccessTokenHeader) {
-		        				config.headers['X-Access-Token'] = accessToken;
-		        			} else {
-		        				config.url = config.url + "?token=" + accessToken;
-		        			}
-		        		}
+
 		        		return config || $q.when(config);
 		        	}
 		        };
@@ -87,7 +97,7 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xed
 		   
 		} ]
 		
-	).run(function($rootScope, $location, $cookieStore, UserService,editableOptions) {
+	).run(function($rootScope, $location, $cookieStore,	editableOptions) {
 
 	     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 		
@@ -95,60 +105,95 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xed
 		$rootScope.$on('$viewContentLoaded', function() {
 			delete $rootScope.error;
 		});
-		
-		$rootScope.hasRole = function(role) {
-			
-			if ($rootScope.user === undefined) {
-				return false;
-			}
-			
-			if ($rootScope.user.roles[role] === undefined) {
-				return false;
-			}
-			
-			return $rootScope.user.roles[role];
-		};
-		
-		$rootScope.logout = function() {
-			delete $rootScope.user;
-			delete $rootScope.accessToken;
-			$cookieStore.remove('accessToken');
-			$location.path("/login");
-		};
-		
+
+
 		 /* Try getting valid user from cookie or go to login page */
 		var originalPath = $location.path();
-		$location.path("/login");
-		var accessToken = $cookieStore.get('accessToken');
-		if (accessToken !== undefined) {
-			$rootScope.accessToken = accessToken;
-			UserService.get(function(user) {
-				$rootScope.user = user;
-				$location.path(originalPath);
-			});
-		}
-		
+		$location.path("/firstpage");
+
 		$rootScope.initialized = true;
 	});
 
 
+//
+//function IndexController($scope, BlogPostService) {
+//
+//	$scope.blogPosts = BlogPostService.query();
+//
+//	$scope.deletePost = function (blogPost) {
+//		blogPost.$remove(function () {
+//			$scope.blogPosts = BlogPostService.query();
+//		});
+//	};
+//}
 
-function IndexController($scope, BlogPostService) {
 
-	$scope.blogPosts = BlogPostService.query();
+function AfterDyeingController($scope,$routeParams, $location, NewMeasurementService) {
 
-	$scope.deletePost = function (blogPost) {
-		blogPost.$remove(function () {
-			$scope.blogPosts = BlogPostService.query();
+	$scope.newMeasurementEntry = new NewMeasurementService();
+	$scope.newMeasurementEntry = NewMeasurementService.get({id: $routeParams.id});
+
+	 $scope.sizes = [
+        {id: 1, s: 'XS'},
+        {id: 2, s: 'S'},
+        {id: 3, s: 'M'},
+        {id: 4, s: 'L'},
+        {id: 5, s: 'XL'}
+      ];
+
+	$scope.save = function() {
+		$scope.newMeasurementEntry.$save(function() {
+			$location.path('/');
 		});
 	};
-}
+};
 
+function AfterPresettingController($scope,$routeParams, $location, NewMeasurementService) {
+
+	$scope.newMeasurementEntry = new NewMeasurementService();
+	$scope.newMeasurementEntry = NewMeasurementService.get({id: $routeParams.id});
+
+	 $scope.sizes = [
+        {id: 1, s: 'XS'},
+        {id: 2, s: 'S'},
+        {id: 3, s: 'M'},
+        {id: 4, s: 'L'},
+        {id: 5, s: 'XL'}
+      ];
+
+	$scope.save = function() {
+		$scope.newMeasurementEntry.$save(function() {
+			$location.path('/');
+		});
+	};
+};
 
 function NewMeasurementController($scope,$routeParams, $location, NewMeasurementService) {
 
 	$scope.newMeasurementEntry = new NewMeasurementService();
 	$scope.newMeasurementEntry = NewMeasurementService.get({id: $routeParams.id});
+
+	 $scope.sizes = [
+        {id: 1, s: 'XS'},
+        {id: 2, s: 'S'},
+        {id: 3, s: 'M'},
+        {id: 4, s: 'L'},
+        {id: 5, s: 'XL'}
+      ];
+
+	$scope.save = function() {
+		$scope.newMeasurementEntry.$save(function() {
+			$location.path('/');
+		});
+	};
+};
+
+function BeforePresettingController($scope,$routeParams, $location
+//, NewMeasurementService
+) {
+
+//	$scope.newMeasurementEntry = new NewMeasurementService();
+//	$scope.newMeasurementEntry = NewMeasurementService.get({id: $routeParams.id});
 
 	 $scope.sizes = [
         {id: 1, s: 'XS'},
@@ -177,15 +222,36 @@ function ViewMeasurementsController($scope, NewMeasurementService) {
 	};
 };
 
-function EditController($scope, $routeParams, $location, BlogPostService) {
+//function EditController($scope, $routeParams, $location, BlogPostService) {
+//
+//	$scope.blogPost = BlogPostService.get({id: $routeParams.id});
+//
+//	$scope.save = function() {
+//		$scope.blogPost.$save(function () {
+//			$location.path('/');
+//		});
+//	};
+//}
 
-	$scope.blogPost = BlogPostService.get({id: $routeParams.id});
-	
-	$scope.save = function() {
-		$scope.blogPost.$save(function () {
-			$location.path('/');
-		});
-	};
+function FirstPageController($scope, $routeParams, $location) {
+}
+
+
+function SelectStageController($scope, $rootScope) {
+
+$rootScope.stage = null;
+$rootScope.status = null;
+if($rootScope.status == beforePresettingStage)    {
+    $rootScope.stage = beforePresettingStage;
+    }
+if($rootScope.status == afterPresettingStage)    {
+    $rootScope.stage = beforePresettingStage;
+    }
+if($rootScope.status == afterDyeingStage)    {
+    $rootScope.stage = beforePresettingStage;
+    }
+console.log($rootScope.stage);
+
 }
 
 
@@ -201,191 +267,131 @@ function CreateController($scope, $location, BlogPostService) {
 };
 
 
-function LoginController($scope, $rootScope, $location, $cookieStore, UserService) {
-	
-	$scope.rememberMe = false;
-	
-	$scope.login = function() {
-		UserService.authenticate($.param({username: $scope.username, password: $scope.password}), function(authenticationResult) {
-			var accessToken = authenticationResult.token;
-			$rootScope.accessToken = accessToken;
-			if ($scope.rememberMe) {
-				$cookieStore.put('accessToken', accessToken);
-			}
-			UserService.get(function(user) {
-				$rootScope.user = user;
-				$location.path("/");
-			});
-		});
-	};
-	};
+//function LoginController($scope, $rootScope, $location, $cookieStore, UserService) {
+//
+//	$scope.rememberMe = false;
+//
+//	$scope.login = function() {
+//		UserService.authenticate($.param({username: $scope.username, password: $scope.password}), function(authenticationResult) {
+//			var accessToken = authenticationResult.token;
+//			$rootScope.accessToken = accessToken;
+//			if ($scope.rememberMe) {
+//				$cookieStore.put('accessToken', accessToken);
+//			}
+//			UserService.get(function(user) {
+//				$rootScope.user = user;
+//				$location.path("/");
+//			});
+//		});
+//
+//
+//	};
+
 
 function EditableTableCtrl($scope, $filter, $http, $q){
- $scope.entries = [
-    {id: 1, gmt: 'Garment1',chestWidth: 0, hemWidth:0,cbLength:0,cfLength:0},
-    {id: 2, gmt: 'Garment2'},
-    {id: 3, gmt: 'Garment3'},
-    {id: 4, gmt: 'Garment4'},
-    {id: 5, gmt: 'Garment5'}
-  ];
+     $scope.entries = [
+        {id: 1, gmt: 'Garment1',chestWidth: 0, hemWidth:0,cbLength:0,cfLength:0},
+        {id: 2, gmt: 'Garment2'},
+        {id: 3, gmt: 'Garment3'},
+        {id: 4, gmt: 'Garment4'},
+        {id: 5, gmt: 'Garment5'}
+      ];
 
-  $scope.statuses = [
-    {value: 1, text: 'status1'},
-    {value: 2, text: 'status2'},
-    {value: 3, text: 'status3'},
-    {value: 4, text: 'status4'}
-  ];
-
-//  $scope.measurements = [
-//    {value: 1,text: 'Chest Width'},
-//    {value: 2,text: 'Hem Width'},
-//    {value: 3,text: 'CB Length'},
-//    {value: 4,text: 'CF Length'},
-//    {value: 5,text: 'Sleeve Opening'},
-//    {value: 1,text: 'Sleeve Length'},
-//    {value: 1,text: 'Sleeve Width'},
-//  ];
-
-
-//  $scope.groups = [];
-//  $scope.loadGroups = function() {
-//    return $scope.groups.length ? null : $http.get('/groups').success(function(data) {
-//      $scope.groups = data;
-//    });
-//  };
-//
-//  $scope.showGroup = function(entry) {
-//    if(entry.group && $scope.groups.length) {
-//      var selected = $filter('filter')($scope.groups, {id: entry.group});
-//      return selected.length ? selected[0].text : 'Not set';
-//    } else {
-//      return entry.groupName || 'Not set';
-//    }
-//  };
-//
-//  $scope.showStatus = function(entry) {
-//    var selected = [];
-//    if(entry.status) {
-//      selected = $filter('filter')($scope.statuses, {value: entry.status});
-//    }
-//    return selected.length ? selected[0].text : 'Not set';
-//  };
-
-//  $scope.checkName = function(data, id) {
-//    if (id === 2 && data !== 'awesome') {
-//      return "Username 2 should be `awesome`";
-//    }
-//  };
-
-  // filter users to show
-//  $scope.filterEntry = function(entry) {
-//    return entry.isDeleted !== true;
-//  };
-//
-//  // mark user as deleted
-//  $scope.deleteEntry = function(id) {
-//    var filtered = $filter('filter')($scope.entry, {id: id});
-//    if (filtered.length) {
-//      filtered[0].isDeleted = true;
-//    }
-//  };
-//
-//  // add user
-//  $scope.addEntry = function() {
-//    $scope.entries.push({
-//      id: $scope.entries.length+1,
-//      measurement: '',
-//      status: null,
-//      group: null,
-//      isNew: true
-//    });
-//  };
-
-  // cancel all changes
-//  $scope.cancel = function() {
-//    for (var i = $scope.entries.length; i--;) {
-//      var user = $scope.entries[i];
-//      // undelete
-//      if (entry.isDeleted) {
-//        delete entry.isDeleted;
-//      }
-//      // remove new
-//      if (entry.isNew) {
-//        $scope.entries.splice(i, 1);
-//      }
-//    };
-//  };
+      $scope.statuses = [
+        {value: 1, text: 'status1'},
+        {value: 2, text: 'status2'},
+        {value: 3, text: 'status3'},
+        {value: 4, text: 'status4'}
+      ];
 
   // save edits
-  $scope.saveTable = function() {
-    var results = [];
-    console.log($scope.entries);
+      $scope.saveTable = function() {
+        var results = [];
+        console.log($scope.entries);
 
-//    for (var i = $scope.entries.length; i--;) {
-//      var user = $scope.entries[i];
-//      // actually delete user
-//      if (entry.isDeleted) {
-//        $scope.entries.splice(i, 1);
-//      }
-//      // mark as not new
-//      if (entry.isNew) {
-//        entry.isNew = false;
-//      }
-//
-//      // send on server
-//      results.push($http.post('/saveEntry', entry));
-//    }
-//
-//    return $q.all(results);
-  };
+        };
 }
 
 
+
+
+function BatchListCtrl($scope){
+     $scope.batches = [
+        {id: 1, batchNo: '12341',styleName: 'ab111'},
+        {id: 2, batchNo: '12342',styleName: 'ab112'},
+        {id: 3, batchNo: '12343',styleName: 'ab113'},
+        {id: 4, batchNo: '12344',styleName: 'ab112'},
+        {id: 5, batchNo: '12345',styleName: 'ab112'}
+
+      ];
+
+      console.log($scope.batches);
+
+
+      $scope.setSelected = function(index) {
+          $scope.selected = $scope.ingredients[index];
+          console.log($scope.selected);
+      };
+//      $scope.batches = BatchListService.query();
+
+     $scope.setSelected = function() {
+             $scope.selected = this.batch.id;
+             console.log($scope.selected);
+         };
+
+
+   }
+
 function AddSleeveCtrl($scope, $filter, $http, $q){
- $scope.entries = [
-    {id: 1, gmt: 'Sleeve1'},
-    {id: 2, gmt: 'Sleeve2'},
-    {id: 3, gmt: 'Sleeve3'},
-    {id: 4, gmt: 'Sleeve4'},
-    {id: 5, gmt: 'Sleeve5'}
-  ];
+     $scope.entries = [
+        {id: 1, gmt: 'Sleeve1'},
+        {id: 2, gmt: 'Sleeve2'},
+        {id: 3, gmt: 'Sleeve3'},
+        {id: 4, gmt: 'Sleeve4'},
+        {id: 5, gmt: 'Sleeve5'}
+      ];
 
-    // save edits
-  $scope.saveTable = function() {
-    var results = [];
-    for (var i = $scope.entries.length; i--;) {
-      var user = $scope.entries[i];
-      // actually delete user
-      if (entry.isDeleted) {
-        $scope.entries.splice(i, 1);
-      }
-      // mark as not new
-      if (entry.isNew) {
-        entry.isNew = false;
-      }
+        // save edits
+      $scope.saveTable = function() {
+        var results = [];
+        for (var i = $scope.entries.length; i--;) {
+          var user = $scope.entries[i];
+          // actually delete user
+          if (entry.isDeleted) {
+            $scope.entries.splice(i, 1);
+          }
+          // mark as not new
+          if (entry.isNew) {
+            entry.isNew = false;
+          }
 
-      // send on server
-      results.push($http.post('/saveEntry', entry));
-    }
+          // send on server
+          results.push($http.post('/saveEntry', entry));
+        }
 
-    return $q.all(results);
-  };
+        return $q.all(results);
+      };
 }
 
 
 var services = angular.module('exampleApp.services', ['ngResource']);
 
-services.factory('UserService', function($resource) {
-	
-	return $resource('rest/user/:action', {},
-			{
-				authenticate: {
-					method: 'POST',
-					params: {'action' : 'authenticate'},
-					headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-				}
-			}
-		);
+//services.factory('UserService', function($resource) {
+//
+//	return $resource('rest/user/:action', {},
+//			{
+//				authenticate: {
+//					method: 'POST',
+//					params: {'action' : 'authenticate'},
+//					headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+//				}
+//			}
+//		);
+//});
+
+services.factory('BatchListService', function($resource) {
+
+	return $resource('rest/new/:id', {id: '@id'});
 });
 
 services.factory('NewMeasurementService', function($resource) {
